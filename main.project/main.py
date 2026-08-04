da911ecf7faa637e917b24c1712e3b898a36923e
zestymec <m.umer394aziz@gmail.com>
2026-08-04 17:04:25 +0500

    Implement main functionality for coffee machine, including menu interaction and resource management; update README with requirements

--- main.project/main.py
+++ main.project/main.py
@@ -1,3 +1,24 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on = True
+menu = Menu()
+
+
+coffee_maker.report()
+money_machine.report()
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like?({options})") 
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(coffee_maker.is_resource_sufficient(drink))
--- main.project/readme.md
+++ main.project/readme.md
@@ -1,11 +1,7 @@
+oop coffee machine 
 Program requirements to do !
 1. print report
 2. check resources sufficient ?
 3. process coins
 4. check transactions succesfull?
 5. make coffee
-
-pretty table
-oop-coffeomachine
-
-112 done
