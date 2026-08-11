3b08bff86efab66e9e74e2f9af66522a5a74bdb9
zestymec <m.umer394aziz@gmail.com>
2026-08-11 16:59:47 +0500

    Implement User class with initialization and update README task count

--- quizProjectonOops/main.py
+++ quizProjectonOops/main.py
@@ -1,3 +1,11 @@
 class User:
-    pass
+    def __init__(self):
+        print("umer")
 user_1 = User()
+user_1.id = "001"
+user_1.uname = "umer"
+
+print(user_1.uname)
+user_2 = User()
+user_2.id = "001"
+user_2.uname = "haumer"
--- readme.md
+++ readme.md
@@ -8,4 +8,4 @@ Program requirements to do !
 pretty table
 oop-coffeomachine
 day 16 done
-117 done
+118 done 4.40
