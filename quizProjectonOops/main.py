e005b16ae8cdebcbda6e7f412a5af637dfb63324
zestymec <m.umer394aziz@gmail.com>
2026-08-12 16:31:55 +0500

    Implement quiz functionality with question model and update README for task completion

--- quizProjectonOops/main.py
+++ quizProjectonOops/main.py
@@ -1,1 +1,11 @@
-umer is a drama king
+from question_model import Question
+from data import question_data
+
+Question_bank = [
+
+]
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text , question_answer)
+    Question_bank.append(new_question)
