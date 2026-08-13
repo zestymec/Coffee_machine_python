3363cee28ffb2831696b9efdfaf40db19a1a8660
zestymec <m.umer394aziz@gmail.com>
2026-08-13 10:48:30 +0500

    Refactor quiz functionality to implement question flow and scoring; update README with task completion

--- quizProjectonOops/main.py
+++ quizProjectonOops/main.py
@@ -9,4 +9,7 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     Question_bank.append(new_question)
 quiz = QuizBrain(Question_bank)
-quiz.next_question()
+while quiz.still_has_question():
+    quiz.next_question()
+print("You have completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- readme.md
+++ readme.md
@@ -8,4 +8,4 @@ Program requirements to do !
 pretty table
 oop-coffeomachine
 day 16 done
-122 done
+125 done
