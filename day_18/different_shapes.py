6cec147821d60eddf018460324df203ef823843f
zestymec <m.umer394aziz@gmail.com>
2026-08-20 10:36:14 +0500

    Add random walk functionality with turtle graphics and update README for progress

--- day_18/different_shapes.py
+++ day_18/different_shapes.py
@@ -1,8 +1,11 @@
 import turtle as t
+import random
 
+colours = ["CornflowerBlue","DarkOrchid", "TndianRed" , "DeepSkyBlue","LightSeaGreen","wheat","SlateGray","SeaGreen"]
 tim = t.Turtle()
 
 for i in range(3, 10):
+    tim.color(random.choice(colours))
     angle = 360 / i
     for _ in range(i):
         tim.forward(100)
--- readme.md
+++ readme.md
@@ -10,4 +10,4 @@ open trivia database !
 pretty table
 oop-coffeomachine
 day 17 done
-132 2.3
+133 done 
