041d529be79743fd91a9a6470b3722543108713a
zestymec <m.umer394aziz@gmail.com>
2026-09-07 18:09:06 +0500

    Refactor main.py for snake game implementation and improve code formatting

--- readme.md
+++ readme.md
@@ -16,4 +16,6 @@ day 18 done
 event listners state , multiple instances
 multiple object and classes 
 oops and turtle to make famous snake game !
-147 3,33
+animation control in python turtle
+time module
+148 15.37
--- snakegame.py/main.py
+++ snakegame.py/main.py
@@ -1,12 +1,49 @@
-from turtle import Turtle , Screen
+from turtle import Turtle, Screen
+import time
 
 screen = Screen()
-screen.setup(width=600 , height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("My snake Game")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-Turtle.shape("box")
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
 
+
+# segment_1 = Turtle("square")
+# segment_1.color("white")
+
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
+
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
+# best way
+
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    
+    for seg_num in range(start= 2, stop= 0, step= -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x , new_y)
+
+ 
+        
 
 
 
