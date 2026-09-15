edc25344d799bea96f809871b143ce58763f2660
zestymec <m.umer394aziz@gmail.com>
2026-09-15 11:14:19 +0500

    Refactor snake game implementation: streamline Snake class initialization and enhance segment color assignment

--- snakegame.py/main.py
+++ snakegame.py/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
 import time
 
@@ -8,29 +8,14 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-snake = Snake()
-
-
-# segment_1 = Turtle("square")
-# segment_1.color("white")
-
-# segment_2 = Turtle("square")
-# segment_2.color("white")
-# segment_2.goto(-20, 0)
-
-# segment_3 = Turtle("square")
-# segment_3.color("white")
-# segment_3.goto(-40, 0)
-# best way
-
-
-
+snake = Snake("Player", "white")
+snake.create_snake()
 
 game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-
+    snake.move()
 
 screen.exitonclick()
--- snakegame.py/snake.py
+++ snakegame.py/snake.py
@@ -4,18 +4,15 @@ starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
 
 class Snake:
-
     def __init__(self, name, color):
         self.name = name
         self.color = color
         self.segments = []
 
-        print(name)
-
     def create_snake(self):
         for position in starting_position:
             new_segment = Turtle("square")
-            new_segment.color("white")
+            new_segment.color(self.color)
             new_segment.penup()
             new_segment.goto(position)
             self.segments.append(new_segment)
@@ -25,4 +22,5 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
+
         self.segments[0].forward(20)
